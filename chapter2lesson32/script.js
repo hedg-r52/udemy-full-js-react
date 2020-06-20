44bc5a9f0108cff0f1b830958801577c9c04b0bb
Andrei Soloviev <hedg.r52@gmail.com>
2020-06-20 21:01:19 +0300

    DOM Navigation

---
+++ chapter2lesson32/script.js
@@ -0,0 +1,14 @@
+// body tag
+//console.log(document.body);
+
+// head tag
+//console.log(document.head);
+
+// html tag
+//console.log(document.documentElement);
+
+// body's childNodes
+//console.log(document.body.childNodes);
+
+console.log(document.body.firstChild);
+console.log(document.body.lastChild);
